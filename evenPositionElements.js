20b93d7b9d69bbfe1486030186f47694faecad88
Desislava Kracheva <desislava.92@abv.bg>
2021-12-27 21:22:08 +0200

    Arrays and Nested Arrays

---
+++ evenPositionElements.js
@@ -0,0 +1,16 @@
+function solve(arr){
+
+    let newArr= [];
+for(let i = 0; i< arr.length; i++){
+
+    if(i%2==0){
+        newArr.push(arr[i]);
+    }
+}
+
+console.log(newArr.join(' '));
+
+
+}
+
+solve(['20', '30', '40', '50', '60'])
