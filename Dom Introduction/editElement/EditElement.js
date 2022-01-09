ce6ed06d142d764c1347b7040aa98206f60b775f
Desislava Kracheva <desislava.92@abv.bg>
2022-01-09 16:39:44 +0200

    Dom Introduction

--- Dom Introduction/editElement/EditElement.js
+++ Dom Introduction/editElement/EditElement.js
@@ -1,9 +1,10 @@
-function editElement() {
-    function edit(ref, match, replacer){
-const content = ref.textContent;
+function edit(ref, match, replacer){
+
 const matcher = new RegExp(match, 'g');
-const edited = content.replace(matcher, replacer)
-ref.textContent = edited;
+const result = ref.textContent.replace(matcher,replacer);
+ref.textContent = result;
+
 
     }
-}
+
+    edit ('el1', 'DOM', 'Document object model')
