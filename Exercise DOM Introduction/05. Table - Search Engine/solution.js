ce6594ed6dae81ec9566a37a48f0c5119c020c63
Desislava Kracheva <desislava.92@abv.bg>
2022-01-12 13:13:14 +0200

    Dom Introduction

--- Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,25 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
+   const rows = document.querySelectorAll('tbody tr');
+
    function onClick() {
-      //   TODO:
+    const input = document.getElementById('searchField').value.toLowerCase();
+
+    for(let row of rows){
+
+      if(row.textContent.toLocaleLowerCase().includes(input)){
+
+row.setAttribute('class', 'select');
+
+      } else {
+
+         row.removeAttribute('class');
+      }
+      
+
+    }
+
 
    }
 }
