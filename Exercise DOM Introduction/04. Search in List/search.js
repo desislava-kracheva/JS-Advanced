aac2eea429445d6979b5c0276111c168dd0bd901
Desislava Kracheva <desislava.92@abv.bg>
2022-01-13 12:54:05 +0200

    Exercise Dom Introduction

--- Exercise DOM Introduction/04. Search in List/search.js
+++ Exercise DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,26 @@
 function search() {
-   // TODO
+ let listItems = document.querySelectorAll('#towns>li');
+ let searchInput = document.getElementById('searchText').value;
+ 
+
+ let result = document.getElementById('result');
+
+ let sum = 0;
+
+ for(let list of listItems){
+if(list.textContent.toLowerCase().includes(searchInput.toLowerCase())){
+
+   list.style.textDecoration = 'underline';
+   list.style.fontWeight = 'bold';
+sum ++;
+} else {
+   list.style.textDecoration = '';
+   list.style.fontWeight = '';
+
+}
+
+
+ }
+ result.textContent = `${sum} matches found`;
+
 }
