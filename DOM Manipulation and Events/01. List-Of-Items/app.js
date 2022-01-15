18d5978fd8b08bb72e57dd5237dddcbdde77cb74
Desislava Kracheva <desislava.92@abv.bg>
2022-01-15 18:03:43 +0200

    DOM Manipulation and Events

---
+++ DOM Manipulation and Events/01. List-Of-Items/app.js
@@ -0,0 +1,12 @@
+function addItem() {
+
+    let newItemText = document.getElementById('newItemText').value;
+    let items = document.getElementById('items');
+    let liElement = document.createElement('li');
+    liElement.textContent = newItemText
+    items.appendChild(liElement);
+    newItemText = '';
+
+
+
+}
