3515b02b2d45ccdc7540ebf670cc0dc58d6aac65
Desislava Kracheva <desislava.92@abv.bg>
2022-01-19 23:38:11 +0200

    Dom Manipulations and Events

--- DOM Manipulation and Events/03. Add-Delete/app.js
+++ DOM Manipulation and Events/03. Add-Delete/app.js
@@ -1,3 +1,30 @@
 function addItem() {
-    //TODO...
-}
+
+    let newItemTextElement = document.getElementById('newItemText')
+    let itemsElement = document.getElementById('items');
+    
+    let  liItemElement = document.createElement ('li');
+    
+    liItemElement.textContent = newItemTextElement.value ;
+    
+    let deleteButton = document.createElement('a');
+    deleteButton.setAttribute('href', '#');
+    
+    deleteButton.textContent = '[Delete]';
+    liItemElement.appendChild(deleteButton);
+    itemsElement.appendChild(liItemElement);
+    newItemTextElement.value = '';
+    deleteButton.addEventListener('click', (e) => {
+    
+    e.currentTarget.parentNode.remove();
+    
+    } )
+    
+    
+    
+    
+    
+    
+    }
+    
+    
