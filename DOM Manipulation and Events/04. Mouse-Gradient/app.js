1d007b9a2a248f5e9bf105e174688fc060906f50
Desislava Kracheva <desislava.92@abv.bg>
2022-01-23 00:36:31 +0200

    Dom Manipulations and Events

--- DOM Manipulation and Events/04. Mouse-Gradient/app.js
+++ DOM Manipulation and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  document.getElementById('gradient').addEventListener('mousemove', onMove);
+
+
+  function onMove(event){
+
+    const offSetX = event.pageX - event.target.offsetLeft;
+    const percent = Math.round(offSetX / event.target.clientWidth * 100);
+
+ document.getElementById('result').textContent = `${percent}%`
+
+  }
 }
