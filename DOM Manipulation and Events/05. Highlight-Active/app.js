4c65cc35728ada1bae1ecbd4042e49fed949119e
Desislava Kracheva <desislava.92@abv.bg>
2022-01-24 00:12:00 +0200

    Dom Manipulations and Events

--- DOM Manipulation and Events/05. Highlight-Active/app.js
+++ DOM Manipulation and Events/05. Highlight-Active/app.js
@@ -1,6 +1,6 @@
 function focused() {
    
-[...document.querySelectorAll('input')].forEach(i => {
+Array.from(document.querySelectorAll('input')).forEach(i => {
 
     i.addEventListener('focus', onFocus);
     i.addEventListener('blur', onBlur);
