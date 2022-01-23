2ccb3a0698767c6127340d4cb6b1da237209a42f
Desislava Kracheva <desislava.92@abv.bg>
2022-01-23 17:12:31 +0200

    Dom Manipulations and Events

--- DOM Manipulation and Events/06. Dynamic-Validation/app.js
+++ DOM Manipulation and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+   
+document.getElementById('email').addEventListener('change', onChange);
+
+function onChange(ev){
+
+    const email = ev.target.value;
+    if(/^[a-z]+@[a-z]+\.[a-z]+$/.test(email)){
+ev.target.className = '';
+
+    } else {
+
+        ev.target.className = 'error'
+    }
+}
+
 }
