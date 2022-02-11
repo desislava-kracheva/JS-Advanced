1e09af87fc08029936b83471b3529ecbb0861e4b
Desislava Kracheva <desislava.92@abv.bg>
2022-02-11 23:35:47 +0200

    new ex.

---
+++ Advanced Functions/areaAndVolumeCalculator.js
@@ -0,0 +1,38 @@
+
+    function area() {
+        return Math.abs(this.x * this.y);
+    };
+    
+
+    function vol() {
+        return Math.abs(this.x * this.y * this.z);
+    };
+    function solve(area, vol, input){
+
+let coordinatesArr = JSON.parse(input);
+let resultArr = [];
+
+for(const coordinates of coordinatesArr){
+    let resultObj = {
+area:area.call(coordinates),
+volume: vol.call(coordinates)
+
+
+    }
+
+    resultArr.push(resultObj);
+}
+return resultArr;
+
+    }
+    solve(area, vol, `[
+        {"x":"10","y":"-22","z":"10"},
+        {"x":"47","y":"7","z":"-5"},
+        {"x":"55","y":"8","z":"0"},
+        {"x":"100","y":"100","z":"100"},
+        {"x":"55","y":"80","z":"250"}
+        ]`
+        );
+
+
+    
